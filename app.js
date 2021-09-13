a957e1c7285723cd6c631e4325981309ba073886
hakim-az <68968670+hakim-az@users.noreply.github.com>
2021-09-13 17:37:44 +0100

    Fixing the navbar on scrolling

--- app.js
+++ app.js
@@ -119,3 +119,18 @@ window.onload = function() {
         }
     })
 }
+
+ // FIXING THE NAVBAR IN SCROLL
+ const nav = document.querySelector('#main');
+ let topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
+    }
+}
+window.addEventListener('scroll', fixNav);
